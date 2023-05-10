12ffe930f511638be995f47749c71c4e6be17a72
guru07 <guru07@atcomputing.nl>
2023-05-10 11:51:00 +0200

    check in copy file

---
+++ Gasunie/helloWorldCopy.cs
@@ -0,0 +1,10 @@
+// Hello World! GUH program
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Worldcopy!");
+        }
+    }
+}
